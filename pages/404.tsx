1790b9041771584be3a769be1e4a5fa2cb32b0f9
toandangcong <toan.dang-cong@banvien.com.vn>
2022-09-28 22:47:20 +0700

    UI 404 error page

---
+++ pages/404.tsx
@@ -0,0 +1,33 @@
+import React, { ReactElement } from "react";
+import ErrorImg from "public/assets/404.png";
+import { NextPageWithLayout } from "./_app";
+import ImageComponent from "src/components/commons/Image";
+import Center from "src/components/commons/Center";
+import Button from "src/components/commons/Button";
+import { useRouter } from "next/router";
+import { Path } from "src/utils/Path";
+import Box from "src/components/commons/Box";
+
+const ErrorPage: NextPageWithLayout = () => {
+  const router = useRouter();
+
+  return (
+    <Center
+      padding="20px 0px 150px"
+      flexDirection="column"
+      width="100vw"
+      height="100vh"
+    >
+      <ImageComponent src="/assets/404.png" />
+      <Box as={Button} width="200px" onClick={() => router.push(Path.home)}>
+        Go Back
+      </Box>
+    </Center>
+  );
+};
+
+ErrorPage.getLayout = function getLayout(page: ReactElement) {
+  return page;
+};
+
+export default ErrorPage;
