400dc44576cc54391dfc1e3e471d1130b70d5057
toandangcong <toan.dang-cong@banvien.com.vn>
2022-10-26 22:39:57 +0700

    finish course view

--- src/containers/CourseDetailContainer/index.tsx
+++ src/containers/CourseDetailContainer/index.tsx
@@ -10,6 +10,10 @@ import HalfStarIcon from "src/components/icons/HalfStarIcon";
 import formatNumber from "src/utils/helpers/formatNumber";
 import ImageComponent from "src/components/commons/Image";
 import CourseContents from "./components/CourseContents/CourseContents";
+import Button from "src/components/commons/Button";
+import PlayIcon from "src/components/icons/PlayIcon";
+import PaperIcon from "src/components/icons/PaperIcon";
+import Center from "src/components/commons/Center";
 
 const course = {
   id: "1",
@@ -32,80 +36,157 @@ const course = {
 
 const CourseDetailContainer = () => {
   return (
-    <Box as={Row} width="80%" margin="0 auto">
-      <Box as={Col} bg="white" borderRadius="large" span={24} padding="15px">
-        <Box
-          as={Text}
-          padding="5px 0"
-          fontSize="xl"
-          lineHeight="xl"
-          fontWeight="bold"
-          color="text"
-        >
-          {course.name}
-        </Box>
-        <Box display="flex" alignItems="baseline">
-          <Box display="flex" alignItems="center">
-            <Text fontSize="base" fontWeight="bold" color="primary">
-              {course.rating}
-            </Text>
-            <Box margin="0 0 0 5px" as="ul" display="flex">
-              {new Array(Number(String(course.rating).split(".")[0]))
-                .fill(1)
-                .map((star, index) => (
-                  <StarIcon key={index} width={15} height={15} />
-                ))}
-              {Number(String(course.rating).split(".")[1]) >= 5 ? (
-                <HalfStarIcon width={15} height={15} />
-              ) : null}
+    <Box as={Row} width="100%" gutter={[10, 0]}>
+      <Col span={17}>
+        <Box bg="white" borderRadius="large" padding="15px">
+          <Box
+            width="max-content"
+            padding="5px 15px"
+            borderRadius="large"
+            bg="green"
+            margin="5px 0 15px"
+          >
+            <Text color="white">{course.category.name}</Text>
+          </Box>
+          <Box
+            as={Text}
+            padding="5px 0"
+            fontSize="xl"
+            lineHeight="xl"
+            fontWeight="bold"
+            color="text"
+          >
+            {course.name}
+          </Box>
+          <Box display="flex" alignItems="baseline">
+            <Box display="flex" alignItems="center">
+              <Text fontSize="base" fontWeight="bold" color="primary">
+                {course.rating}
+              </Text>
+              <Box margin="0 0 0 5px" as="ul" display="flex">
+                {new Array(Number(String(course.rating).split(".")[0]))
+                  .fill(1)
+                  .map((star, index) => (
+                    <StarIcon key={index} width={15} height={15} />
+                  ))}
+                {Number(String(course.rating).split(".")[1]) >= 5 ? (
+                  <HalfStarIcon width={15} height={15} />
+                ) : null}
+              </Box>
+            </Box>
+            <Box as={Text} padding="0 0 0 10px">
+              ({formatNumber(course.students)} students)
             </Box>
           </Box>
-          <Box as={Text} padding="0 0 0 10px">
-            ({formatNumber(course.students)} students)
-          </Box>
-        </Box>
-        <Box
-          padding="5px 0"
-          fontSize="sm"
-          fontWeight="regular"
-          lineHeight="large"
-          color="text"
-        >
-          Created By{" "}
           <Box
-            as={Link}
-            href={`${Path.user}/${course.author.id}`}
-            fontWeight="bold"
-            color="green"
+            padding="5px 0"
+            fontSize="sm"
+            fontWeight="regular"
+            lineHeight="large"
+            color="text"
           >
-            <Text
-              style={{ cursor: "pointer" }}
-              as="span"
+            Created By{" "}
+            <Box
+              as={Link}
+              href={`${Path.user}/${course.author.id}`}
               fontWeight="bold"
               color="green"
             >
-              {course.author.name}
+              <Text
+                style={{ cursor: "pointer" }}
+                as="span"
+                fontWeight="bold"
+                color="green"
+              >
+                {course.author.name}
+              </Text>
+            </Box>
+          </Box>
+          <Box margin="25px 0" width="100%" height="250px">
+            <ImageComponent
+              src="/assets/thumbnail-example.png"
+              alt="thumbnail"
+            />
+          </Box>
+          <Box margin="15px 0">
+            <Text fontSize="xl" lineHeight="xl" fontWeight="bold" color="text">
+              Description:
             </Text>
+            <Box>{parse(JSON.parse(course.description))}</Box>
+          </Box>
+          <Box margin="25px 0">
+            <Text fontSize="xl" lineHeight="xl" fontWeight="bold" color="text">
+              Course Content:
+            </Text>
+            <Box margin="10px 0 0">
+              <CourseContents />
+            </Box>
           </Box>
         </Box>
-        <Box margin="25px 0" width="100%" height="250px">
-          <ImageComponent src="/assets/thumbnail-example.png" alt="thumbnail" />
-        </Box>
-        <Box margin="15px 0">
-          <Text fontSize="xl" lineHeight="xl" fontWeight="bold" color="text">
-            Description:
-          </Text>
-          <Box>{parse(JSON.parse(course.description))}</Box>
-        </Box>
-        <Box margin="25px 0">
-          <Text fontSize="xl" lineHeight="xl" fontWeight="bold" color="text">
-            Course Content:
-          </Text>
-          <Box margin="10px 0 0">
-            <CourseContents />
+      </Col>
+      <Col span={7}>
+        <Box bg="white" borderRadius="large" padding="15px">
+          <Box width="100%" height="200px" style={{ position: "relative" }}>
+            <ImageComponent
+              src="/assets/thumbnail-example.png"
+              alt="thumbnail"
+            />
+            <Center
+              style={{ position: "absolute", top: 0, left: 0 }}
+              width="100%"
+              height="100%"
+              bg="rgba(0,0,0, 0.2)"
+              borderRadius="large"
+            >
+              <Center
+                width="40px"
+                height="40px"
+                bg="primary"
+                borderRadius="rounded"
+              >
+                <PlayIcon />
+              </Center>
+            </Center>
+          </Box>
+          <Box
+            fontSize="xl"
+            fontWeight="bold"
+            color="text"
+            lineHeight="xl"
+            textAlign="center"
+            as={Text}
+            padding="10px 0"
+          >
+            19.99$
+          </Box>
+          <Box as={Button} width="100%" margin="15px 0">
+            Enroll Now
+          </Box>
+          <Box as="ul">
+            <Text
+              fontSize="sm"
+              fontWeight="bold"
+              color="text"
+              lineHeight="large"
+              margin="0 0 10px"
+            >
+              This course includes:
+            </Text>
+            <Box as="li" display="flex" padding="8px 0">
+              <Box width="30px">
+                <PlayIcon />
+              </Box>
+              <Text>11 hours on-demand video</Text>
+            </Box>
+            <Box as="li" display="flex" margin="8px 0">
+              <Box width="30px">
+                <PaperIcon />
+              </Box>
+              <Text>3 sections</Text>
+            </Box>
           </Box>
         </Box>
-      </Box>
+      </Col>
     </Box>
   );
 };
