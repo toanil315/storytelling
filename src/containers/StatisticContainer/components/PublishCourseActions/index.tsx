f3a49112846d0ef8596e7f87068897d1d89c8c5d
toandangcong <toan.dang-cong@banvien.com.vn>
2022-12-22 21:50:16 +0700

    show error when submit without informations, fix notifications list

--- src/containers/StatisticContainer/components/PublishCourseActions/index.tsx
+++ src/containers/StatisticContainer/components/PublishCourseActions/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Box from "src/components/commons/Box";
 import EditIcon from "src/components/icons/EditIcon";
 import DeleteIcon from "src/components/icons/DeleteIcon";
+import EyeIcon from "src/components/icons/ViewIcon";
 import { useRouter } from "next/router";
 import { Path } from "src/utils/Path";
 
@@ -15,6 +16,19 @@ const PublishCourseAction = ({ record }: Props) => {
   return (
     <Box display="flex">
       <Box
+        className="cursor-pointer"
+        padding="5px"
+        borderRadius="md"
+        border="1px solid"
+        borderColor="primary"
+        bg="primary"
+        margin="0 5px 0 0 "
+        onClick={() => router.push(`${Path.courses}/${record.id}`)}
+      >
+        <EyeIcon fill="white" />
+      </Box>
+      <Box
+        className="cursor-pointer"
         padding="5px"
         borderRadius="md"
         border="1px solid"
@@ -27,6 +41,7 @@ const PublishCourseAction = ({ record }: Props) => {
       </Box>
       <Box>
         <Box
+          className="cursor-pointer"
           padding="5px"
           borderRadius="md"
           border="1px solid"
--- src/hooks/apis/Course/useAccessCourseByLectureId.tsx
+++ src/hooks/apis/Course/useAccessCourseByLectureId.tsx
@@ -6,6 +6,7 @@ const useAccessCourseByLectureId = () => {
   const getCourseByLectureId = async (lectureId: string) => {
     setIsLoading(true);
     const { data: lectureData } = await courseService.getLectureById(lectureId);
+    console.log("lectureID: ", lectureId, lectureData);
     const { data: sectionData } = await courseService.getSectionById(
       lectureData.sectionId
     );
